128a9bb1da5f43cb14e3d22b36ce7a26d08e67c7
Xaivntaaj Her <dn_her@yahoo.com>
2023-05-18 16:01:12 -0500

    setup navbar and searchbar function

---
+++ react/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = (props) => {
+    return ( 
+        <header>
+            <h1>Music<small className="text-muted">Library</small></h1>
+        </header>
+     );
+}
+ 
+export default NavBar;
---
+++ react/src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,21 @@
+
+
+const SearchBar = (props) => {
+    return ( 
+        <form>
+            <div>
+                <div>
+                    <input>List</input>
+                    <input>Search</input>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <button type="submit">Search</button>
+                </div>
+            </div>
+        </form>
+     );
+}
+ 
+export default SearchBar;
